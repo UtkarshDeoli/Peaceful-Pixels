aaa0c658b17cc186957df0d163cacc42ce0eab9d
UtkarshDeoli <utkarsh.deoli@gmail.com>
2024-04-07 02:14:31 +0530

    Basic Operations

--- IoTPixels/IotCode/IotCode.ino
+++ IoTPixels/IotCode/IotCode.ino
@@ -1,12 +1,133 @@
-const int LED_PIN = 2; // GPIO2 is the built-in LED pin on ESP01
+#include <ESP8266WiFi.h> 
+#include <PubSubClient.h>
+#define MAX_MSG_LEN (128)
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define buzzer 3
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+const char* ssid = "Pandhari";
+const char* password = "Deoli@wifi";
+const char *serverHostname = "broker.hivemq.com";
+//const IPAddress serverIPAddress(192, 168, 1, 9);
+const char *topic = "test/utkarsh/india/LedState";
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 
 void setup() {
-    pinMode(LED_PIN, OUTPUT);
+  // Configure serial port for debugging
+  Serial.begin(9600);
+  Wire.begin(2, 0); 
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);// initialize with the I2C addr 0x3D (for the 128x64)
+  // Initialise wifi connection - this will wait until connected
+  connectWifi();
+  // connect to MQTT server  
+  client.setServer(serverHostname, 1883);
+  client.setCallback(callback);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  pinMode(buzzer,OUTPUT);
 }
 
 void loop() {
-    digitalWrite(LED_PIN, HIGH); // Turn on the LED
-    delay(1000); // Wait for 1 second
-    digitalWrite(LED_PIN, LOW); // Turn off the LED
-    delay(1000); // Wait for 1 second
+    if (!client.connected()) {
+      connectMQTT();
+    }
+    // this is ESSENTIAL!
+    client.loop();
+    // idle
+    delay(500);
 }
+
+void connectWifi() {
+  delay(10);
+  // Connecting to a WiFi network
+  Serial.printf("\nConnecting to %s\n", ssid);
+  display.println("Connecting....");
+  display.display();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected on IP address ");
+  display.println("Connected:) IP:");
+  display.println(WiFi.localIP());
+  Serial.println(WiFi.localIP());
+}
+
+// connect to MQTT server
+void connectMQTT() {
+  // Wait until we're connected
+  while (!client.connected()) {
+    // Create a random client ID
+    String clientId = "ESP8266-";
+    clientId += String(random(0xffff), HEX);
+    Serial.printf("MQTT connecting as client %s...\n", clientId.c_str());
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("MQTT connected");
+      display.println("MQTT connected");
+      display.display();
+      digitalWrite(buzzer,HIGH);
+      delay(1000);
+      digitalWrite(buzzer,LOW);
+      delay(1000);
+      display.clearDisplay();
+      // Once connected, publish an announcement...
+      client.publish(topic, "Device is Connected");
+      // ... and resubscribe
+      client.subscribe(topic);
+    } else {
+      Serial.printf("MQTT failed, state %s, retrying...\n", client.state());
+      // Wait before retrying
+      delay(2500);
+    }
+  }
+}
+
+void callback(char *msgTopic, byte *msgPayload, unsigned int msgLength){
+  // copy payload to a static string
+  static char message[MAX_MSG_LEN+1];
+  if (msgLength > MAX_MSG_LEN) {
+    msgLength = MAX_MSG_LEN;
+  }
+  strncpy(message, (char *)msgPayload, msgLength);
+  message[msgLength] = '\0';
+  display.clearDisplay();
+  display.setCursor(0,20);
+  display.println(message);
+  display.display();
+  Serial.printf("topic %s, message received: %s\n", topic, message);
+  digitalWrite(buzzer,HIGH);
+  delay(100);
+  digitalWrite(buzzer,LOW);
+  if (strcmp(message, "FUCK U") == 0) {
+    fuckMe();
+  }
+  }
+
+
+void fuckMe()
+{
+  for(int i=0;i<10;i++)
+  {
+    digitalWrite(buzzer,HIGH);
+    delay(1000 -i*10);
+    digitalWrite(buzzer,LOW);
+  }
+  }
--- peaceful_pixels/lib/main.dart
+++ peaceful_pixels/lib/main.dart
@@ -1,125 +1,253 @@
 import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
 
-void main() {
-  runApp(const MyApp());
+final client = MqttServerClient('broker.hivemq.com', '1883');
+void main() async {
+  client.logging(on: true);
+  client.keepAlivePeriod = 60;
+  client.onConnected = onConnected;
+  try {
+    await client.connect();
+  } on NoConnectionException catch (e) {
+    print('client exception - $e');
+    client.disconnect();
+  }
+  if (client.connectionStatus!.state == MqttConnectionState.connected) {
+    print('client connected');
+  } else {
+    print(
+        'client connection failed - disconnecting, status is ${client.connectionStatus}');
+    client.disconnect();
+  }
+  runApp(MyApp(client: client));
+}
+
+void onConnected() {
+  const pubTopic = 'test/utkarsh/india/LedState';
+  final builder = MqttClientPayloadBuilder();
+  builder.addString('Hello from mqtt_client');
+  client.publishMessage(pubTopic, MqttQos.exactlyOnce, builder.payload!);
+}
+
+void sendMessage(String ans) {
+  const pubTopic = 'test/utkarsh/india/LedState';
+  final builder = MqttClientPayloadBuilder();
+  builder.addString(ans);
+  client.publishMessage(pubTopic, MqttQos.exactlyOnce, builder.payload!);
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final MqttServerClient client;
+  const MyApp({super.key, required this.client});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String ans = "";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 35, 35, 35),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Peaceful Pixels"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 25, 20, 50),
+            child: TextField(
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.w200),
+              decoration: const InputDecoration(
+                hintText: 'Enter the Custom Message You Wanna Send',
+                hintStyle: TextStyle(color: Colors.white38),
+                icon: Icon(Icons.mail),
+              ),
+              onChanged: (value) {
+                // Update the ans variable when the text changes
+                setState(() {
+                  ans = value;
+                });
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          ElevatedButton(
+            child: const Text("Press to Send"),
+            onPressed: () {
+              sendMessage(ans);
+              final snackBar = SnackBar(content: Text('Custom Message Sent !'));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            },
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      sendMessage("I Love You");
+                      final snackBar =
+                          SnackBar(content: Text('Message Sent !'));
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Colors.teal,
+                      ),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.favorite,
+                            size: 60,
+                            color: Colors.red,
+                          ),
+                          Text(
+                            "Press To Send Love",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                //second one
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      sendMessage("FUCK U");
+                      final snackBar =
+                          SnackBar(content: Text('Message Sent !'));
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Colors.redAccent,
+                      ),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.notification_important_sharp,
+                            size: 60,
+                            color: Colors.yellow,
+                          ),
+                          Text(
+                            "Emergency",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      sendMessage("Miss You");
+                      final snackBar =
+                          SnackBar(content: Text('Message Sent !'));
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Colors.pink,
+                      ),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.sms,
+                            size: 60,
+                            color: Color.fromARGB(255, 131, 189, 255),
+                          ),
+                          Text(
+                            "Miss You",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      sendMessage("Come When You Have Time");
+                      final snackBar = SnackBar(
+                        content: Text('Message Sent !'),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Colors.redAccent,
+                      ),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.notifications,
+                            size: 60,
+                            color: Colors.yellow,
+                          ),
+                          Text(
+                            "Come Soon",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
